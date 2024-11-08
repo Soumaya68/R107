083cdc9413e35902ed0be491ee779abb2d2c442f
Soumaya68 <soumaya.rabah68@gmail.com>
2024-11-08 16:18:50 +0100

    maj

--- essai.py
+++ essai.py
@@ -1,1 +1,14 @@
-essaibhjbhjbh
+x = int(input("Entrez x : "))
+y = int(input("Entrez y : "))
+
+print("Avant permutation:")
+print("x :", x)
+print("y :", y)
+
+t=x
+x=y
+y=t
+
+print("Après permutation:")
+print("x:",  x)
+print("y:",  y)
