503794b902bd01c14b9166f90d1f8d538200683b
Soumaya68 <soumaya.rabah68@gmail.com>
2024-11-22 16:02:44 +0100

    maj

---
+++ TP2/exercice 4.py
@@ -0,0 +1,20 @@
+BASE = int(4)
+fromage = float(800.0)
+eau = float(2.0)
+ail = float(2.0)
+pain = float(400.0)
+
+nbConvives = int(input("Entrez le nombre de personne(s) conviées à la fondue : "))
+
+nouvelleQuantitéfromage = fromage * nbConvives / BASE
+nouvelleQuantitéeau = eau * nbConvives / BASE
+nouvelleQuantitéail = ail * nbConvives / BASE
+nouvelleQuantitépain = pain * nbConvives / BASE
+
+
+print("Pour faire une fondue fribourgeoise pour" , nbConvives , "personnes, il vous faut : ")
+print(nouvelleQuantitéfromage ,"gr de fromage")
+print(nouvelleQuantitéeau ,"dl d'eau")
+print(nouvelleQuantitéail ,"gousse(s) d'ail")
+print(nouvelleQuantitépain ,"gr de pain")
+
