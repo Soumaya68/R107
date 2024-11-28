ebaca86991e1e6e8af8a148314b354a706849c89
e2400066 <soumaya.rabah68@gmail.com>
2024-11-28 09:00:59 +0100

    maj

--- TP2/exercice 8.py
+++ TP2/exercice 8.py
@@ -1,8 +1,6 @@
 x = float(input("Entrez un nombre décimal : "))
 
-I = ((2 == x or x > 2) and x < 3) or \
-    (0 < x and (x == 1 or x < 1)) or \
-    (-10 <= x and x < -2)
+I = (x >= 2  and x < 3) or (0 < x and x <= 1) or (-10 <= x and x < -2)
 
 if I :
     print("x appartient à I")
